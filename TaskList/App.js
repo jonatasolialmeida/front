aad24e4ddbfa26df4b1ae613404320431225a764
jonatas.almeida <jonatas.almeida@globalhitss.com.br>
2023-08-31 20:39:53 -0300

    criação do projeto com expo ++ componente principal Task ++ styles

--- TaskList/App.js
+++ TaskList/App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Task de Hoje</Text>
+
+        <View style={styles.items}>
+          <Task text={'Primeira Task'}/>
+          <Task text={'Segunda Task'}/>
+          <Task text={'Terceira Task'}/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +22,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#202020',
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  items: {
+    marginTop: 30,
   },
 });
